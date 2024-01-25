4c0be0dc142300ae77320dd8c2076d9bdc56b3e0
Imre <decodeimre@gmail.com>
2024-01-25 09:19:08 +0100

    almost done - cleanup needed

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+//buy tickets form
+
 function openBookingForm() {
     document.getElementById('booking-form-overlay').style.display = 'flex';
   }
@@ -7,3 +9,30 @@ function openBookingForm() {
   }
   
   document.getElementById('book-tickets-btn').addEventListener('click', openBookingForm);
+  document.getElementById('book-tickets-btn-two').addEventListener('click', openBookingForm);
+
+
+//switch between content
+
+function openLineUp() {
+    document.getElementById('line-up').style.display = 'block';
+    document.getElementById('festival-infos').style.display = 'none';
+}
+// Get all Elements with the class name "line-up-btn"
+let lineUpBtns = document.getElementsByClassName('line-up-btn');
+// Add event listener to each element with class 'line-up-btn'
+for (let i = 0; i < lineUpBtns.length; i++) {
+  lineUpBtns[i].addEventListener('click', openLineUp);
+}
+
+
+
+function openInfos() {
+  document.getElementById('line-up').style.display = 'none';
+  document.getElementById('festival-infos').style.display = 'block';
+}
+let infoBtns = document.getElementsByClassName('info-btn');
+
+for (let i = 0; i < infoBtns.length; i++) {
+  infoBtns[i].addEventListener('click', openInfos);
+}
